08fdcea0b1998add78286e3c6a3ea110987ef455
EvelinaK <evelinalg79@gmail.com>
2020-12-02 00:34:35 +0200

    1

--- src/pages/MoviePage.js
+++ src/pages/MoviePage.js
@@ -3,12 +3,21 @@ import details from '../tamplates/movie.hbs';
 
 const init = async params => {
   const API = new MovieAPI();
-
   const movie = await API.getMoviesById(params.id);
   console.log(movie);
+
+  //-------отвечает за смену банера. можно вынести в services
+  const refs = {
+    banner: document.querySelector('.page-header'),
+  };
+  refs.banner.className = 'page-header';
+  refs.banner.classList.add('banner-detalis');
+  //-------отвечает за смену банера. можно вынести в services
 
   // Обязательно возврашщать разметку
   return details(movie);
 };
+
+// elem.classList.toggle("class")
 
 export default init;
